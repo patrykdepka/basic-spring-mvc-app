531870ebbc59ec850759dc61890987918dde56c4
Patryk Depka <patrykdepka@gmail.com>
2023-02-04 23:30:19 +0100

    App generated by Spring Initializr

---
+++ src/main/java/pl/patrykdepka/basicspringmvcapp/BasicSpringMvcAppApplication.java
@@ -0,0 +1,13 @@
+package pl.patrykdepka.basicspringmvcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringMvcAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringMvcAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/patrykdepka/basicspringmvcapp/BasicSpringMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patrykdepka.basicspringmvcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringMvcAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
