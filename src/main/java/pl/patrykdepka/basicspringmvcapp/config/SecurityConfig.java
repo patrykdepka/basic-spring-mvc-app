a4f03ef0c83dd377bfe15f1d0024475e81caf690
Patryk Depka <patrykdepka@gmail.com>
2023-02-06 21:25:17 +0100

    Added custom authentication failure messages to login form

--- src/main/java/pl/patrykdepka/basicspringmvcapp/config/SecurityConfig.java
+++ src/main/java/pl/patrykdepka/basicspringmvcapp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import pl.patrykdepka.basicspringmvcapp.appuser.CustomAuthenticationFailureHandler;
 
 @Configuration
 public class SecurityConfig {
@@ -12,8 +13,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests -> requests
+                .mvcMatchers("/login**").permitAll()
                 .anyRequest().authenticated());
-        http.formLogin(login -> login.loginPage("/login").permitAll());
+        http.formLogin(login -> login
+                .loginPage("/login").permitAll()
+                .failureHandler(new CustomAuthenticationFailureHandler())
+        );
         http.csrf().disable();
         http.headers().frameOptions().disable();
         return http.build();
