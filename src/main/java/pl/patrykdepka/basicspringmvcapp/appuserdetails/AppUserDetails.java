bfc28319eb1e089ec930cc3ff2f9bf38634d7087
Patryk Depka <patrykdepka@gmail.com>
2023-02-07 22:34:41 +0100

    Created main page and navbar

--- src/main/java/pl/patrykdepka/basicspringmvcapp/appuserdetails/AppUserDetails.java
+++ src/main/java/pl/patrykdepka/basicspringmvcapp/appuserdetails/AppUserDetails.java
@@ -27,6 +27,22 @@ public class AppUserDetails extends User {
         this.lastName = appUserDetailsBuilder.lastName;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public static final class AppUserDetailsBuilder {
         private String firstName;
         private String lastName;
--- src/main/java/pl/patrykdepka/basicspringmvcapp/config/SecurityConfig.java
+++ src/main/java/pl/patrykdepka/basicspringmvcapp/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package pl.patrykdepka.basicspringmvcapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import pl.patrykdepka.basicspringmvcapp.appuser.CustomAuthenticationFailureHandler;
 
 @Configuration
@@ -13,11 +15,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests -> requests
+                .mvcMatchers("/").permitAll()
                 .mvcMatchers("/login**").permitAll()
                 .anyRequest().authenticated());
         http.formLogin(login -> login
                 .loginPage("/login").permitAll()
                 .failureHandler(new CustomAuthenticationFailureHandler())
+        );
+        http.logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
+                .logoutSuccessUrl("/login?logout").permitAll()
         );
         http.csrf().disable();
         http.headers().frameOptions().disable();
